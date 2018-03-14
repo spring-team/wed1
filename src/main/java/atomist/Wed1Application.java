646c72510a7564cdea1b48cdfa64cba2a8650685
Rod Johnson <johnsonroda@gmail.com>
2018-03-14 11:15:18 +1100

    Initial commit from Atomist

---
+++ src/main/java/atomist/Wed1Application.java
@@ -0,0 +1,12 @@
+package atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wed1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wed1Application.class, args);
+	}
+}
